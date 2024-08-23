9e7a886a86aed765dbc7346bc175e4a67c68f19c
Genesis Guerrero <genesis.guerrero@venturatravel.org>
2024-08-23 09:30:01 +0200

    increase body size limit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverActions: {
+    bodySizeLimit: "100mb",
+  },
   experimental: {
+    serverActionsBodySizeLimit: "100mb",
     serverActions: {
       bodySizeLimit: "100mb",
     },
